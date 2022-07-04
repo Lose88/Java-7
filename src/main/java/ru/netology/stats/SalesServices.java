3b60f043bafa9eeb4d78cfc3e8202b9fc68ad5e7
Lose88 <hotsting1888@gmail.com>
2022-07-04 11:25:05 +0300

    addChainges

--- src/main/java/ru/netology/stats/SalesServices.java
+++ src/main/java/ru/netology/stats/SalesServices.java
@@ -1,2 +1,64 @@
-package ru.netology.stats;public class SalesServices {
+package ru.netology.stats;
+
+public class SalesServices {
+    public long sumSales(long[] price) {
+
+        int sum = 0;
+        for (long sale : price) {
+            sum += sale;
+        }
+        return sum;
+    }
+
+    public long average(long[] price) {
+        return sumSales(price) / price.length;
+    }
+
+    public long maxSales(long[] price) {
+        int maxMonth = 0;
+        int month = 0;
+        for (long sale : price) {
+
+            if (sale >= price[maxMonth]) {
+                maxMonth = month;
+            }
+            month = month + 1;
+        }
+        return maxMonth + 1;
+    }
+
+    public long minSales(long[] price) {
+        int minMonth = 0;
+        int month = 0;
+        for (long sale : price) {
+
+            if (sale <= price[minMonth]) {
+                minMonth = month;
+            }
+            month = month + 1;
+        }
+        return minMonth + 1;
+    }
+
+    public int salesUpAverage(long[] price) {
+        long average = average(price);
+        int salesUp = 0;
+        for (long sale : price) {
+            if (sale > average) {
+                salesUp++;
+            }
+        }
+        return salesUp;
+    }
+
+    public int salesBelowAverage(long[] price) {
+        long average = average(price);
+        int salesBelow = 0;
+        for (long sale : price) {
+            if (sale < average) {
+                salesBelow++;
+            }
+        }
+        return salesBelow;
+    }
 }
--- src/test/java/ru/netology/stats/SalesServicesTest.java
+++ src/test/java/ru/netology/stats/SalesServicesTest.java
@@ -1,40 +1,65 @@
+package ru.netology.stats;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import ru.netology.stats.SalesServices;
-package ru.netology.stats;
+
+
 public class SalesServicesTest {
     @Test
     public void sumTest() {
         SalesServices services = new SalesServices();
-        int[] price = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long[] price = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         int expected = 180;
-        int actual = services.sumSales(price);
+        long actual = services.sumSales(price);
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
     public void averageTest() {
+
+        long[] price = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         SalesServices services = new SalesServices();
-        int[] price = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
-        int expected = 15;
-        int actual = services.sumSales(price) / price.length;
+        long expected = 15;
+        long actual = services.average(price);
         Assertions.assertEquals(expected, actual);
     }
-    @Test
-    public void maxSalesMonth(){
-SalesServices services = new SalesServices();
-        int[] price = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
-        int maxMonth = 0;
-        int month = 0; // переменная для индекса рассматриваемого месяца в массиве
-        for (long sale : price) {
-            // sales[minMonth] - продажи в месяце minMonth
-            // sale - продажи в рассматриваемом месяце
-            if (sale >= price[maxMonth]) {
-                maxMonth = month;
-            }
-            month = month + 1; // следующий рассматриваемый месяц имеет номер на 1 больше
-        }
 
+    @Test
+    public void maxSales() {
+        long[] price = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        SalesServices services = new SalesServices();
+        int expected = 8;
+        long actual = services.maxSales(price);
+        Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    public void minSales() {
+        long[] price = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        SalesServices services = new SalesServices();
+        int expected = 9;
+        long actual = services.minSales(price);
+        Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    public void salesUpAverage() {
+        long[] price = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        SalesServices services = new SalesServices();
+        int expected = 5;
+        long actual = services.salesUpAverage(price);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void salesBelowAverage() {
+        long[] price = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        SalesServices services = new SalesServices();
+        int expected = 5;
+        long actual = services.salesBelowAverage(price);
+        Assertions.assertEquals(expected, actual);
+    }
+}
+
 
